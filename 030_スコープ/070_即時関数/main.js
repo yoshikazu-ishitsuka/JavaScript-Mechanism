b486defaf5be22bdd0f45d16459cca91e55841a5
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-07-15 17:14:30 +0900

    即時関数

--- 030_スコープ/070_即時関数/main.js
+++ 030_スコープ/070_即時関数/main.js
@@ -4,10 +4,31 @@ function a() {
 
 a();
 
-let c = (function (d) {
-  console.log("called " + d);
-  return 0;
-})(10);
+let c = (function () {
+  console.log("called");
+
+  let privateVal = 0;
+  let publicVal = 10;
+
+  function privateFn() {
+    console.log("privateFn is called");
+  }
+  function publicFn() {
+    console.log("publicFn is called" + privateVal++);
+  }
+
+  return {
+    publicVal,
+    publicFn,
+  };
+})();
+
+c.publicFn();
+c.publicFn();
+c.publicFn();
+c.publicFn();
+
+console.log(c.publicVal);
 
 let b = (function () {
   console.log("called");
