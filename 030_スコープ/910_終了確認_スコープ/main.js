9837ca288f7e21a6986c0a7648c103f72252c5bd
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-07-17 20:28:52 +0900

    issue1

--- 030_スコープ/910_終了確認_スコープ/main.js
+++ 030_スコープ/910_終了確認_スコープ/main.js
@@ -3,14 +3,15 @@
  * 以下のコードではエラーが発生します。
  * コンソールで"fn called"と表示されるように
  * fn内のコードを変更してください。
- * 
+ *
  * ※if文は削除してはいけません。
  */
 function fn() {
-    if(true) {
-        let a = 'fn called';
-    }
-    return a; // ReferenceError: a is not defined
+  let a;
+  if (true) {
+    a = "fn called";
+  }
+  return a; // ReferenceError: a is not defined
 }
 
 const result = fn();
@@ -21,16 +22,16 @@ console.log(result);
  * fn2内の記述を変更して、各コンソールで
  * 期待値を出力するように修正してください。
  */
-var val = 'val1';
+var val = "val1";
 function fn2() {
-    console.log(val); // 期待値->'val1'
+  console.log(val); // 期待値->'val1'
 
-    if(true) {
-        var val = 'val2';
-        console.log(val); // 期待値->'val2'
-    }
+  if (true) {
+    var val = "val2";
+    console.log(val); // 期待値->'val2'
+  }
 
-    console.log(val); // 期待値->'val1'
+  console.log(val); // 期待値->'val1'
 }
 fn2();
 
@@ -39,10 +40,9 @@ fn2();
  * 「クロージャー（プライベート変数）」のレクチャーで作成
  * したincrementと同じ機能を持つincrement関数をブロック
  * スコープとクロージャーを利用して作成してみてください。
- * 
+ *
  * increment(); // 期待値->1
  * increment(); // 期待値->2
  * increment(); // 期待値->3
  * increment(); // 期待値->4
  */
-
