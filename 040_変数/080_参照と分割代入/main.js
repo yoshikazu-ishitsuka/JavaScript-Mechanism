341ac986fe05ac013ad6b3fa34000bdfa0fd6507
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-08-18 15:57:15 +0900

    参照と分割代入-2

--- 040_変数/080_参照と分割代入/main.js
+++ 040_変数/080_参照と分割代入/main.js
@@ -5,4 +5,25 @@ const a = {
 let { prop } = a;
 
 prop = 1;
+
 console.log(a, prop);
+
+function fn({ prop }) {
+  prop = 1;
+  console.log(a, prop);
+}
+
+fn(a);
+
+const c = {
+  prop1: {
+    prop2: 0,
+  },
+};
+
+let { prop1 } = c;
+console.log(prop1);
+
+prop1.prop2 = 1;
+
+console.log(c, prop1);
