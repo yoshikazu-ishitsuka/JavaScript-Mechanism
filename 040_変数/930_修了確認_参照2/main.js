904662c694241332ff44d41cd0bb346b555ff311
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-08-30 19:52:06 +0900

    修了確認_参照2 問題1

--- 040_変数/930_修了確認_参照2/main.js
+++ 040_変数/930_修了確認_参照2/main.js
@@ -6,15 +6,14 @@
  * わっていませんでした。
  * 以下のminus関数をどのように修正すればobj.prop1
  * の値を変更することができるでしょうか？
- * 
+ *
  */
 let obj = {
-    prop1: 10
-}
+  prop1: 10,
+};
 
 function minus(obj, val) {
-    let prop1 = obj.prop1;
-    prop1 = prop1 - val;
+  obj.prop1 = obj.prop1 - val;
 }
 
 minus(obj, 1);
@@ -25,19 +24,17 @@ console.log(obj.prop1);
  * と思っていました。しかし、コンソールに表示された
  * のは元の値である'9'でした。
  * どうすれば、'18'が表示されるようになるでしょうか？
- * 
+ *
  * '18'が表示されるように、double関数内を修正してください。
  */
 
-
 function double(obj) {
-    let { prop1 } = obj;
-    prop1 = prop1 * 2;
+  let { prop1 } = obj;
+  prop1 = prop1 * 2;
 }
 
 double(obj);
 console.log(obj.prop1);
-
 
 /**
  * 問題３：
@@ -47,14 +44,14 @@ console.log(obj.prop1);
  * 考えてみてください。
  */
 obj.prop2 = {
-    prop3: 1
-}
+  prop3: 1,
+};
 
 function fn({ prop2 }) {
-    let prop = prop2;
-    prop.prop3 = 2;
-    prop = { prop3: 3 };
-    return { prop2: prop };
+  let prop = prop2;
+  prop.prop3 = 2;
+  prop = { prop3: 3 };
+  return { prop2: prop };
 }
 obj = fn(obj);
 // console.log(obj.prop2.prop3);
@@ -64,8 +61,8 @@ obj = fn(obj);
  * through関数を経由して格納されるobj2は
  * objと等価でしょうか？
  */
-function through (obj) {
-    return obj;
+function through(obj) {
+  return obj;
 }
 
 const obj2 = through(obj);
