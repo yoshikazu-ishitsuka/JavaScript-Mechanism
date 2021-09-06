310c39ebbf7a72ff83d838a1ed811a8258562036
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-09-06 12:50:25 +0900

    関数4

--- 050_関数とオブジェクト1/010_関数/main.js
+++ 050_関数とオブジェクト1/010_関数/main.js
@@ -1,14 +1,14 @@
-function fn(...args) {
-  console.log(args);
+function fn() {
   const a = arguments[0];
   const b = arguments[1];
   console.log(arguments);
   console.log(a, b);
+  return a;
 }
 
 // function fn(a) {
 //   console.log(2);
 // }
 
-let c = null;
-fn(1, undefined, 0);
+let c = fn(1, undefined);
+console.log(c);
