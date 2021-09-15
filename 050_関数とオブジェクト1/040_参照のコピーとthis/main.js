4e37d0095d1487a9007df90244cfbf3f71e21f2e
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-09-15 18:02:46 +0900

    参照のコピーと'this' -2

--- 050_関数とオブジェクト1/040_参照のコピーとthis/main.js
+++ 050_関数とオブジェクト1/040_参照のコピーとthis/main.js
@@ -1,8 +1,17 @@
+window.name = "John";
+
 const person = {
   name: "Tom",
   hello: function () {
     console.log("Hello " + this.name);
+    a();
   },
 };
-const ref = person.hello;
-ref();
+// const ref = person.hello;
+// ref();
+
+person.hello();
+
+function a() {
+  console.log("Hello " + this.name);
+}
