c4d24d13879b8c53bdd35a88fae449a4d32ed3c9
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-09-20 18:53:20 +0900

    bindと'this' -1

--- 050_関数とオブジェクト1/050_コールバック関数とthis/main.js
+++ 050_関数とオブジェクト1/050_コールバック関数とthis/main.js
@@ -8,8 +8,18 @@ const person = {
 };
 person.hello();
 
+const helloTom = person.hello.bind(person);
+
 function fn(ref) {
   ref();
 }
 
-fn(person.hello);
+fn(helloTom);
+
+function a() {
+  console.log("hello " + this.name);
+}
+
+const b = a.bind({ name: "Tim" });
+
+b();
