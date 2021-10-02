90a37d73b34cacdb2260d5de09f5753631bcffe0
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-10-02 18:37:08 +0900

    アロー関数 -3

--- 050_関数とオブジェクト1/080_アロー関数/main.js
+++ 050_関数とオブジェクト1/080_アロー関数/main.js
@@ -2,6 +2,6 @@ function a(name) {
   return "hello " + name;
 }
 
-const b = (name) => "hello " + name;
+const b = (name, name1) => "hello " + name + " " + name1;
 
-console.log(b("Tom"));
+console.log(b("Tom", "Bob"));
