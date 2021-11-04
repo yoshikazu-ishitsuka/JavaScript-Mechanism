f26a1298f219a6e70282e2d3c33c77786bcbcbbe
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-11-04 20:47:54 +0900

    prototype -2

--- 050_関数とオブジェクト2/020_prototype/main.js
+++ 050_関数とオブジェクト2/020_prototype/main.js
@@ -1,11 +1,14 @@
 function Person(name, age) {
   this.name = name;
   this.age = age;
+  this.hello = function () {
+    console.log("hello " + this.name);
+  };
 }
 
-Person.prototype.hello = function () {
-  console.log("hello " + this.name);
-};
+// Person.prototype.hello = function () {
+//   console.log("hello " + this.name);
+// };
 
 const bob = new Person("Bob", 18);
 const tom = new Person("Tom", 33);
