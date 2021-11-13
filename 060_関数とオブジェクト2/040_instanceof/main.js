c875091cb5c6d371b51e2d0fa69e48f7bc628638
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-11-13 22:55:13 +0900

    instanceof -2

--- 060_関数とオブジェクト2/040_instanceof/main.js
+++ 060_関数とオブジェクト2/040_instanceof/main.js
@@ -1,10 +1,13 @@
 function F(a, b) {
   this.a = a;
   this.b = b;
-  // return { a: 1 };
+  const result = new Object();
+  result.a = 1;
+  return result;
 }
 
 F.prototype.c = function () {};
 
 const instance = new F(1, 2);
 console.log(instance instanceof F);
+console.log(instance.__proto__ === Object.prototype);
