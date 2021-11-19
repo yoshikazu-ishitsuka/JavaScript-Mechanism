dfb6160a42401d8eea023538420180facd3b1e01
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-11-19 21:14:10 +0900

    関数コンストラクター -2

--- 060_関数とオブジェクト2/050_関数コンストラクター/main.js
+++ 060_関数とオブジェクト2/050_関数コンストラクター/main.js
@@ -1,4 +1,7 @@
-const fn1 = new Function("a", "b", "return a + b");
+function fn() {
+  let d = 1;
+  const fn1 = new Function("a", "b", "return a + b * d");
+}
 
 const result = fn1(1, 2);
 
@@ -7,3 +10,5 @@ console.log(result);
 function fn2(a, b) {
   return a + b;
 }
+
+console.log(fn2 instanceof Function);
