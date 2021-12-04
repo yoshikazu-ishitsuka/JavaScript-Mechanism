b6d60824b11b65a105a02f0aa1cb0ff4bcfdca63
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-12-04 19:43:53 +0900

    hasOwnPropertyとin -6

--- 060_関数とオブジェクト2/070_hasOwnPropertyとin/main.js
+++ 060_関数とオブジェクト2/070_hasOwnPropertyとin/main.js
@@ -10,4 +10,4 @@ Object.prototype.hello = function () {
 const bob = new Person("Bob", 18);
 const result = bob.hasOwnProperty("name");
 console.log(result);
-console.log("hello" in bob);
+console.log("hasOwnProperty" in bob);
