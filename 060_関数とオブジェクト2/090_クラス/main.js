560504c778c376437df0b9a1b208d733203fc14a
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-12-23 19:31:20 +0900

    クラス -7

--- 060_関数とオブジェクト2/090_クラス/main.js
+++ 060_関数とオブジェクト2/090_クラス/main.js
@@ -9,11 +9,14 @@ class Person {
   }
 }
 
+const bob = new Person("Bob", 23);
+console.log(bob);
+
 // function Person(name, age) {
 //   this.name = name;
 //   this.age = age;
 // }
 
-Person.prototype.hello = function () {
-  console.log("hello " + this.name);
-};
+// Person.prototype.hello = function () {
+//   console.log("hello " + this.name);
+// };
