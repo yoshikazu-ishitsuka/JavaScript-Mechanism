a3bf6c6771f52c8ce8ff9e2a448cb1580c79c7b5
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2022-01-04 19:20:14 +0900

    クラス継承 -12

--- 060_関数とオブジェクト2/100_クラス継承/main.js
+++ 060_関数とオブジェクト2/100_クラス継承/main.js
@@ -9,15 +9,6 @@ class Person {
   }
 }
 
-// function Person(name, age) {
-//   this.name = name;
-//   this.age = age;
-// }
-
-// Person.prototype.hello = function () {
-//   console.log("hello " + this.name);
-// };
-
 class Japanese extends Person {
   constructor(name, age, gender) {
     super(name, age);
@@ -32,21 +23,6 @@ class Japanese extends Person {
     console.log("Sayonara " + this.name);
   }
 }
-
-// function Japanese(name, age, gender) {
-//   Person.call(this, name, age);
-//   this.gender = gender;
-// }
-
-// Japanese.prototype = Object.create(Person.prototype);
-
-Japanese.prototype.hello = function () {
-  console.log("Konnichiwa " + this.name);
-};
-
-Japanese.prototype.bye = function () {
-  console.log("Sayonara " + this.name);
-};
 
 const taro = new Japanese("Taro", 23, "Male");
 console.log(taro);
