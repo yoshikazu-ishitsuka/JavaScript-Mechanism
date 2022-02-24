67566312232e26458f85eaa4fefe2c8521f84ddb
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2022-02-24 20:17:03 +0900

    getter, setterとstatic -2

--- 060_関数とオブジェクト2/160_getter,setterとstatic/main.js
+++ 060_関数とオブジェクト2/160_getter,setterとstatic/main.js
@@ -2,3 +2,9 @@ function Person(name, age) {
   this._name = name;
   this._age = age;
 }
+
+Object.defineProperty(Person1.prototype, "name", {
+  get: function () {
+    return this._name;
+  },
+});
