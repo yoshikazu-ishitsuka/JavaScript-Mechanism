682d24a1936c1cdb16e69d1e4784fb7c92965846
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2022-03-10 21:45:35 +0900

    getter, setterとstatic -16

--- 060_関数とオブジェクト2/160_getter,setterとstatic/main.js
+++ 060_関数とオブジェクト2/160_getter,setterとstatic/main.js
@@ -1,16 +1,15 @@
-function Person(name, age) {
+function Person1(name, age) {
   this._name = name;
   this._age = age;
 }
 
-Person1.hello = function() {
-  console.log("hello")
-}
+Person1.hello = function () {
+  console.log("hello");
+};
 Person1.hello();
 
 Object.defineProperty(Person1.prototype, "name", {
   get: function () {
-    console.log("hello");
     return this._name;
   },
   set: function (val) {
@@ -26,22 +25,22 @@ const p1 = new Person1("Bob", 23);
 class Person2 {
   constructor(name, age) {
     this._name = name;
-    this._age = age;;
+    this._age = age;
+  }
 
-    get name() {
-      console.log("hello");
-      return this._name;
-    },
+  get name() {
+    return this._name;
+  }
 
-    set name(val) {
-      this._name = val;
-    }
+  set name(val) {
+    this._name = val;
+  }
 
-    static hello(){
-      console.log("hello")
-    }
+  static hello() {
+    console.log("hello");
+  }
 }
 
-
+const p2 = new Person2("Bob", 23);
 
 Person2.hello();
