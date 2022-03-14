211b1524eccc38f752bcb092ffd1b7e27426b866
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2022-03-14 21:10:36 +0900

    チェーンメソッド -3

--- 060_関数とオブジェクト2/170_チェーンメソッド/main.js
+++ 060_関数とオブジェクト2/170_チェーンメソッド/main.js
@@ -3,8 +3,12 @@ class Person {
     this.name = name;
     this.age = age;
   }
-}
 
-hello(person){
-  console.log(`${this.name} says hello ${person.name}`);
+  hello(person) {
+    console.log(`${this.name} says hello ${person.name}`);
+  }
+
+  introduce() {
+    console.log(`Hi, I'm ${this.name}, ${this.age}`);
+  }
 }
